9cdb52f3caa478ceae5eabcd26bc5063089b25e1
Cao Quoc Tuan <quoctuan.cqt@gmail.com>
2020-05-21 15:35:46 +0700

    Revert config

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -54,49 +54,25 @@ namespace IdentityServer
                 },
                 new Client
                 {
-                    ClientId = "0b791e06-0d97-4fc4-8682-65e18b5d3a8f",
-                    ClientName = "Tenant 1",
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
                     AllowedGrantTypes = GrantTypes.Code,
-                    ClientSecrets = { new Secret("2e9c95d0-19e9-4ee6-9c8b-b65824f64179".Sha256()) },
                     RequireConsent = false,
-                    RedirectUris = {
-                        "http://localhost:5002/signin-oidc",
-                        "http://localhost:5000/swagger/oauth2-redirect.html",
-                    },
-                    PostLogoutRedirectUris = {
-                        "http://localhost:5002/signout-callback-oidc",
-                        "http://localhost:5000/swagger/index.html",
-                    },
-                    AllowedScopes =
+                    RequirePkce = true,
+                
+                    // where to redirect to after login
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
                     {
-                        "openid",
-                        "profile",
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
                         "api_server"
-                    },
-                    Description = "0b791e06-0d97-4fc4-8682-65e18b5d3a8f"
-                },
-                new Client
-                {
-                    ClientId = "435c3b72-b310-495a-8ad6-8de3995492da",
-                    ClientName = "Tenant 2",
-                    AllowedGrantTypes = GrantTypes.Code,
-                    ClientSecrets = { new Secret("089e122c-6f55-4eaa-a2aa-1b7718a2a8de".Sha256()) },
-                    RequireConsent = false,
-                     RedirectUris = {
-                        "http://localhost:5003/signin-oidc",
-                        "http://localhost:5000/swagger/oauth2-redirect.html",
-                    },
-                    PostLogoutRedirectUris = {
-                        "http://localhost:5003/signout-callback-oidc",
-                        "http://localhost:5000/swagger/index.html",
-                    },
-                    AllowedScopes =
-                    {
-                        "openid",
-                        "profile",
-                        "api_server"
-                    },
-                    Description = "435c3b72-b310-495a-8ad6-8de3995492da"
+                    }
                 },
             };
     }
